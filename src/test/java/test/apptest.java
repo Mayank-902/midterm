c1e19e188ff04d5a384cb5dedf678353c7add28d
MAYANK RAJ <mayank.09raj@gmail.com>
2023-10-31 11:15:51 +0530

    updated apptest

--- src/test/java/test/apptest.java
+++ src/test/java/test/apptest.java
@@ -1,5 +1,16 @@
 package test;
 
-public class apptest {
+import static org.junit.Assert.*;
+import org.junit.Test;
 
+public class apptest {
+    @Test
+    public void testValidUser() {
+        assertTrue(app.validate("Shreyas", "Shreyas@123"));
+    }
+
+    @Test
+    public void testInvalidUser() {
+        assertFalse(app.validate("wronguser", "wrongpassword"));
+    }
 }
